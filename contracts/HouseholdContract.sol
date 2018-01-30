a7c392b35d41083c5ddc0c6facb6abb081bc64f2
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2018-01-30 18:43:28 +0200

    Moved more computations to library

--- contracts/HouseholdContract.sol
+++ contracts/HouseholdContract.sol
@@ -26,7 +26,7 @@ contract HouseholdContract{
 	/*
 	* Called hourly or daily to update water _usage
 	**/
-	function addWaterUsage(uint256 _usage) public returns (uint256) {
+	function addWaterUsage(uint256 _usage) public returns (uint256 usage) {
 		cumulativeUsage[msg.sender] = _usage;
 		return cumulativeUsage[msg.sender];
 	}
@@ -34,7 +34,7 @@ contract HouseholdContract{
 	/**
 	* the cumulative water usage resets every month or when the user pays
 	**/
-	function resetWaterUsage() public returns (uint256) {
+	function resetWaterUsage() public returns (uint256 usage) {
 		cumulativeUsage[msg.sender] = 0;
 		//resets the Pi.... --assumption
 		return cumulativeUsage[msg.sender];
@@ -46,45 +46,45 @@ contract HouseholdContract{
 	* Pay totals price and convert remainder to bounty
 	*/
 	function pay(uint256 _recommendedCumulativeUsage, uint256 _bountyFactor) public returns (uint256 voucher, uint256 amount) {
-		uint256 voucher2 = 0;
-		if(_recommendedCumulativeUsage > cumulativeUsage[msg.sender]) {
+		uint256 voucher = 0;
+		if(_recommendedCumulativeUsage >= cumulativeUsage[msg.sender]) {
 			bounty[msg.sender] += HouseholdLibrary.calculateBounty(_recommendedCumulativeUsage, cumulativeUsage[msg.sender]);
-			voucher2 = HouseholdLibrary.calculateVoucher(bounty[msg.sender], _bountyFactor);
+			voucher = HouseholdLibrary.calculateVoucher(bounty[msg.sender], _bountyFactor);
 		}
 		else
-			price[msg.sender] += 1 * (cumulativeUsage - _recommendedCumulativeUsage);
-		uint256 amount2 = cumulativeUsage[msg.sender] * price[msg.sender];
+			price[msg.sender] += HouseholdLibrary.increasePrice(cumulativeUsage[msg.sender], _recommendedCumulativeUsage);
+		uint256 amount = HouseholdLibrary.calculateOutstandingBalance(cumulativeUsage[msg.sender], price[msg.sender], 0);
 		resetWaterUsage();
-		return (voucher2, amount2 / 100);
+		return (voucher, HouseholdLibrary.centToRand(amount));
 	}
 
 	function getOutstandingBalance(uint256 _recommendedCumulativeUsage) returns (uint256 balance){
 		uint256 _factor = 0;
-		if(_recommendedCumulativeUsage > cumulativeUsage[msg.sender])
-			_factor += 1 * (cumulativeUsage[msg.sender] - _recommendedCumulativeUsage);
-		return cumulativeUsage[msg.sender] * (price[msg.sender] + _factor) / 100;
+		if(_recommendedCumulativeUsage < cumulativeUsage[msg.sender])
+			_factor = HouseholdLibrary.increasePenaltyFactor(cumulativeUsage[msg.sender], _recommendedUsage);
+		return HouseholdLibrary.calculateOutstandingBalance(cumulativeUsage[msg.sender], price[msg.sender], _factor);
 	}
 
 	/*
 	* function to lower price if it is high
 	**/
-	function lowerPrice(uint256 factor) returns (uint256 pric) {
+	function lowerPrice(uint256 _factor) returns (uint256 r_price) {
 		if(price[msg.sender] > 1) {
-			price[msg.sender] -= factor;
+			price[msg.sender] -= _factor;
 		}
 
 		return price[msg.sender];
 	}
 
-	function getLowerPriceReq() {
-		return (price[msg.sender] - 1) * 3000;
+	function getLowerPriceReq() public view returns (uint256 amount){
+		return HouseholdLibrary.lowerPriceReq(price[msg.sender])
 	}
 
-	function getWaterUsage() view returns (uint256 usage) {
+	function getWaterUsage() public view returns (uint256 usage) {
 		return cumulativeUsage[msg.sender];
 	}
 
-	function getBounty() view returns (uint256 bountyp) {
+	function getBounty() public view returns (uint256 r_bounty) {
 		return bounty[msg.sender];
 	}
 }
--- contracts/HouseholdLibrary.sol
+++ contracts/HouseholdLibrary.sol
@@ -8,4 +8,28 @@ library HouseholdLibrary {
 	function calculateVoucher(uint256 _bounty, uint256 _factor) public pure returns (uint256 voucher){
 		return _bounty * _factor;
 	}
+
+	function increasePrice(uint256 _usage, _recommendedUsage) public pure returns (uint256 newPrice) {
+		return 1 * (_usage - _recommendedUsage);
+	}
+
+	/*function getAmountToPay(uint256 _usage, uint256 _price) public pure returns (uint256 amount) {
+		return _usage * _price;
+	}*/
+
+	function centToRand(uint256 _cents) public pure returns (uint256 rands) {
+		return _cents / 100;
+	}
+
+	function increasePenaltyFactor(uint256 _usage, uint256 _recommendedUsage) public pure returns (uint256 factor) {
+		return 1 * (_usage - _recommendedUsage);
+	}
+
+	function calculateOutstandingBalance(uint256 _usage, uint256 _price, uint256 _factor) public pure returns (uint256 amount) {
+		return _usage * (_price + _factor) / 100;
+	}
+
+	function lowerPriceReq(uint256 _price) public pure returns (uint256 price){
+		return (_price - 1) * 3000;
+	}
 }

d6748738a749f2f6929666214d29657d089fb701
Nicolaas <nm24991759@gmail.com>
2018-01-30 14:26:09 +0200

    Styles and home page added

--- src/components/styles/Styles.js
+++ src/components/styles/Styles.js
@@ -1,8 +1,22 @@
-const styles = StyleSheet.create({
+import react from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+module.exports = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#f9f9f9',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    cardHeader: {
+      fontSize: 30
+    },
+    cardBody: {
+      fontSize: 25,
+      textAlign: 'center',
+      marginBottom: 10
+    },
+    cardBackground: {
+      
+    }
   });
